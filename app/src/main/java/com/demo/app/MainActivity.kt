dd8f16440e56a4e78db2f3b1daca02dc2ef101ca
ezy <249736246@qq.com>
2024-10-24 15:28:52 +0800

    #

--- app/src/main/java/com/demo/app/MainActivity.kt
+++ app/src/main/java/com/demo/app/MainActivity.kt
@@ -1,43 +1,53 @@
 package com.demo.app
 
-import android.graphics.Typeface
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.core.text.HtmlCompat
 import com.demo.app.databinding.ActivityMainBinding
-import me.reezy.cosmo.tabs.TabItem
-import me.reezy.cosmo.tabs.setup
+import me.reezy.cosmo.html.CustomElementHandler
+import me.reezy.cosmo.html.HTML
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private val binding by lazy { ActivityMainBinding.bind(findViewById<ViewGroup>(android.R.id.content).getChildAt(0)) }
 
-    private val items = arrayOf("html", "inline", "block", "custom")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        binding.pager.offscreenPageLimit = 30
-        binding.pager.adapter = object : FragmentStateAdapter(supportFragmentManager, lifecycle) {
-            override fun createFragment(position: Int): Fragment {
-                return when(position) {
-                    0 -> HtmlFragment()
-                    1 -> InlineFragment()
-                    2 -> BlockFragment()
-                    else -> CustomFragment()
-                }
-            }
+        val html1 = """
+<del>del</del>
+<s>s</s>
+<strike>strike</strike>
 
-            override fun getItemCount(): Int = items.size
-        }
+<h1 style="text-align:center">Heading</h1>
 
-        binding.tabs.setup(items.map { TabItem(it, it) }, binding.pager) {
-            textView?.textSize = 18f
-            textView?.setTypeface(Typeface.DEFAULT, Typeface.BOLD)
-        }
+Agree to the <action name="privacy">Privacy Policy</action> and the <action name="agreement">User Agreement</action>
+
+<blockquote style="text-align:center">blockquote</blockquote>
+
+<p style="text-align:end">You can reach Michael at:</p>
+
+<ul>
+  <li style="color:#ff0000;"><a href="https://example.com">Website</a></li>
+  <li style="text-decoration:line-through;"><a href="mailto:m.bluth@example.com">Email</a></li>
+  <li style="background-color:#ff0000;"><a href="tel:+123456789">Phone</a></li>
+  <li style="color:#ff0000; text-decoration:line-through; background-color:#CC9900;"><a href="tel:+123456789">Test</a></li>
+</ul> 
+        """.trimIndent()
 
 
+
+        binding.hello.movementMethod = LinkMovementMethod.getInstance()
+
+        binding.hello.text = HtmlCompat.fromHtml(html1, HtmlCompat.FROM_HTML_MODE_COMPACT)
+
+        binding.world.movementMethod = LinkMovementMethod.getInstance()
+
+        binding.world.text = HTML.fromHtml(html1, elementHandler = CustomElementHandler {
+            Toast.makeText(this, "action = $it", Toast.LENGTH_SHORT).show()
+        })
     }
 }
